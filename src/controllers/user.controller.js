d2206521773d8ced56d0cad867400eaa743121a4
saurabh yadav <72351102+saurabhyadav01@users.noreply.github.com>
2022-08-04 15:13:45 +0530

    Update user.controller.js

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -10,16 +10,24 @@ router.get("",async(req,res)=>{
         return res.status(500).send(error.message)
     }
 })
-
-router.delete("/:id",async(req,res)=>{
+router.get("/:id",async(req,res)=>{
     try {
-        let users= await User.findByIdAndDelete(res.param.id);
+        let users= await User.findById(req.params.id).lean().exec();
         return res.status(200).send(users);
     } catch (error) {
         return res.status(500).send(error.message)
     }
 })
+
 router.put("/:id",async(req,res)=>{
+    try {
+        let users= await User.findByIdAndDelete(req.params.id)
+        return res.status(200).send("DELETED");
+    } catch (error) {
+        return res.status(500).send(error.message)
+    }
+})
+ router.patch("/:id",async(req,res)=>{
     try {
         let users= await User.findByIdAndUpdate({ name: req.body.name,mobile: req.body.mobile,photo: req.body.photo });
         return res.status(200).send(users);
